343524b76cdc8fe30862784dfbb6000a24788372
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-11 22:22:22 -0700

    Adding Background Circles with Framer Motion

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "../components/BackgroundCircles";
 
 type Props = {};
 
 export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Ivan Gonzalez",
+      "Guy-who-loves-Coffee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
-    <div>
-      <h1>Hero...</h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <h1>
+        <p style={{ fontSize: 20 }}>{text}</p>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
     </div>
   );
 }
